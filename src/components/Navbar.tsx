d151042fa2d868347c5f0bf5ae2a8dccbec43c65
antonypauson <13antonysun@gmail.com>
2025-08-06 22:35:14 +0530

    feat: making the sun/moon icon invisible for other pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import moonIcon from "../assets/moon-icon.svg"; 
 import sunIcon from "../assets/sun-icon.svg"; 
 
@@ -11,9 +11,13 @@ export default function Navbar({setIconTrue, iconTrue}) {
       { name: "Contact", path: "/contact" },
     ];
 
+    const location = useLocation(); 
+
+    const isHomePage = location.pathname === "/"; 
+
     return (
       <nav className="absolute top-0 inset-x-0 z-10 p-4 bg-transparent flex justify-between items-center mx-2 md:mx-5">
-        <div>
+        <div className={!isHomePage ? "invisible" : ""}>
           <img className="w-8 md:w-12 cursor-pointer" src={iconTrue ? sunIcon : moonIcon} onClick={() => setIconTrue(!iconTrue)} />
         </div>
         <div className="flex gap-3 px-1.5 md:gap-5 md:px-5.5 md:text-2xl">
